2f4d8f5971b40a9120bff7ee48a08671fd01fd9c
Paulo_Scarano <scaranopaulo@gmail.com>
2021-05-08 22:49:42 -0300

    Desenvolvendo a página de serviços

---
+++ wp-content/themes/wp-bootstrap-starter-child/page-service-sub.php
@@ -0,0 +1,136 @@
+<?php
+// Template Name: Sub-Serviços
+?>
+<?php get_header(); ?>
+
+        <!--banner-->
+        <div id="banner">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-12 titulo banner-content">                            
+
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/elementos/elipse.svg" id="circle1" alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/elementos/elipse.svg" id="circle2" alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/elementos/dotted.svg" id="dotted1" alt="">
+                            <h1>Serviços</h1>
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/elementos/dotted.svg" id="dotted2" alt="">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/elementos/dotted.svg" id="dotted3" alt="">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/banner-->
+    </header>
+
+    <main>
+        <div class="service">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12-md">
+                        <div class="titulo">
+                            <h1>serviços que prestamos</h1>
+                            <div class="underbar"></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-md-3">
+                        <a href="#">
+                            <div class="card">
+                                <div class="content">
+                                    <h1>01</h1>
+                                    <h3>fotografia</h3>
+                                    <p>
+                                        Precisa de identidade de visual para seu projeto ?
+                                    </p>
+                                    <div class="img-area"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/camera.svg" alt="maquina_digital"></div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-md-3">
+                        <a href="#">
+                            <div class="card">
+                                <div class="content">
+                                    <h1>02</h1>
+                                    <h3>papelaria</h3>
+                                    <p>
+                                        Precisa de identidade de visual para seu projeto ?
+                                    </p>
+                                    <div class="img-area"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/caneta.svg" alt="maquina_digital"></div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-md-3">
+                        <a href="#">
+                            <div class="card">
+                                <div class="content">
+                                    <h1>03</h1>
+                                    <h3>mentoria</h3>
+                                    <p>
+                                        Precisa de identidade de visual para seu projeto ?
+                                    </p>
+                                    <div class="img-area"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mentoria.svg" alt="maquina_digital"></div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>                  
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-md-3">
+                        <a href="#">
+                            <div class="card">
+                                <div class="content">
+                                    <h1>04</h1>
+                                    <h3>midias digitais</h3>
+                                    <p>
+                                        Precisa de identidade de visual para seu projeto ?
+                                    </p>
+                                    <div class="img-area"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/arroba.svg" alt="maquina_digital"></div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-md-3">
+                        <a href="#">
+                            <div class="card">
+                                <div class="content">
+                                    <h1>05</h1>
+                                    <h3>marketing</h3>
+                                    <p>
+                                        Precisa de identidade de visual para seu projeto ?
+                                    </p>
+                                    <div class="img-area"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/marketing.svg" alt="maquina_digital"></div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-md-3">
+                        <a href="#">
+                            <div class="card">
+                                <div class="content">
+                                    <h1>06</h1>
+                                    <h3>identidade visual</h3>
+                                    <p>
+                                        Precisa de identidade de visual para seu projeto ?
+                                    </p>
+                                    <div class="img-area"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/vetor.svg" alt="maquina_digital"></div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>                  
+                </div>
+
+                
+            </div>
+        </div>
+    </main>
+<?php get_footer(); ?>
--- wp-content/themes/wp-bootstrap-starter-child/page-service.php
+++ wp-content/themes/wp-bootstrap-starter-child/page-service.php
@@ -1,136 +1,71 @@
 <?php
 // Template Name: Serviços
 ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_header(); ?>
 
-        <!--banner-->
-        <div id="banner">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-12 titulo banner-content">                            
+            <!--banner-->
+            <div id="banner">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-12 titulo banner-content">                            
 
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/elementos/elipse.svg" id="circle1" alt="">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/elementos/elipse.svg" id="circle2" alt="">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/elementos/dotted.svg" id="dotted1" alt="">
-                            <h1>Serviços</h1>
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/elementos/dotted.svg" id="dotted2" alt="">
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/elementos/dotted.svg" id="dotted3" alt="">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/elementos/elipse.svg" id="circle1" alt="">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/elementos/elipse.svg" id="circle2" alt="">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/elementos/dotted.svg" id="dotted1" alt="">
+                                <h1><?php the_field('titulo_banner') ?></h1>
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/elementos/dotted.svg" id="dotted2" alt="">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/elementos/dotted.svg" id="dotted3" alt="">
 
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--/banner-->
-    </header>
-
-    <main>
-        <div class="service">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12-md">
-                        <div class="titulo">
-                            <h1>serviços que prestamos</h1>
-                            <div class="underbar"></div>
                         </div>
                     </div>
                 </div>
-
-                <div class="row justify-content-center">
-                    <div class="col-md-3">
-                        <a href="#">
-                            <div class="card">
-                                <div class="content">
-                                    <h1>01</h1>
-                                    <h3>fotografia</h3>
-                                    <p>
-                                        Precisa de identidade de visual para seu projeto ?
-                                    </p>
-                                    <div class="img-area"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/camera.svg" alt="maquina_digital"></div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="col-md-3">
-                        <a href="#">
-                            <div class="card">
-                                <div class="content">
-                                    <h1>02</h1>
-                                    <h3>papelaria</h3>
-                                    <p>
-                                        Precisa de identidade de visual para seu projeto ?
-                                    </p>
-                                    <div class="img-area"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/caneta.svg" alt="maquina_digital"></div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="col-md-3">
-                        <a href="#">
-                            <div class="card">
-                                <div class="content">
-                                    <h1>03</h1>
-                                    <h3>mentoria</h3>
-                                    <p>
-                                        Precisa de identidade de visual para seu projeto ?
-                                    </p>
-                                    <div class="img-area"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mentoria.svg" alt="maquina_digital"></div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>                  
-                </div>
-
-                <div class="row justify-content-center">
-                    <div class="col-md-3">
-                        <a href="#">
-                            <div class="card">
-                                <div class="content">
-                                    <h1>04</h1>
-                                    <h3>midias digitais</h3>
-                                    <p>
-                                        Precisa de identidade de visual para seu projeto ?
-                                    </p>
-                                    <div class="img-area"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/arroba.svg" alt="maquina_digital"></div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="col-md-3">
-                        <a href="#">
-                            <div class="card">
-                                <div class="content">
-                                    <h1>05</h1>
-                                    <h3>marketing</h3>
-                                    <p>
-                                        Precisa de identidade de visual para seu projeto ?
-                                    </p>
-                                    <div class="img-area"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/marketing.svg" alt="maquina_digital"></div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="col-md-3">
-                        <a href="#">
-                            <div class="card">
-                                <div class="content">
-                                    <h1>06</h1>
-                                    <h3>identidade visual</h3>
-                                    <p>
-                                        Precisa de identidade de visual para seu projeto ?
-                                    </p>
-                                    <div class="img-area"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/vetor.svg" alt="maquina_digital"></div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>                  
-                </div>
-
-                
             </div>
-        </div>
-    </main>
+            <!--/banner-->
+        </header>
+
+        <main>
+            <div class="service">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12-md">
+                            <div class="titulo">
+                                <h1>
+                                    <?php the_field('servicos') ?>
+                                </h1>
+                                <div class="underbar"></div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <?php if(have_rows('linha_servicos')): while(have_rows('linha_servicos')) : the_row(); ?>
+                        <div class="row justify-content-center">
+
+                            <?php if(have_rows('item_servicos')): while(have_rows('item_servicos')) : the_row(); ?>
+                                <div class="col-md-3">
+                                    <a href="/nic/servicos">
+                                        <div class="card">
+                                            <div class="content">
+                                                <h1><?php the_sub_field('numero_item_servico') ?></h1>
+                                                <h3><?php the_sub_field('nome_item_servico') ?></h3>
+                                                <p>
+                                                    <?php the_sub_field('texto_item_servico') ?>    
+                                                </p>
+                                                <div class="img-area">
+                                                    <img src="<?php the_sub_field('imagem_item_servico') ?>" alt="">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            <?php endwhile; else : endif; ?>
+                                        
+                        </div>
+                    <?php endwhile; else : endif; ?>                      
+                </div>
+            </div>
+        </main>
 <?php get_footer(); ?>
+<?php endwhile; else: endif; ?>
