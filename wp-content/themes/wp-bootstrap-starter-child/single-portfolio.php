88fefbfb1b2572f627e0664fbcba7f2917c35d94
Paulo_Scarano <scaranopaulo@gmail.com>
2021-05-26 23:06:46 -0300

    PostPortfolio

--- wp-content/themes/wp-bootstrap-starter-child/single-portfolio.php
+++ wp-content/themes/wp-bootstrap-starter-child/single-portfolio.php
@@ -12,7 +12,7 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             <style type="text/css">
                 header {
-                    background: url("<?php the_field('imagem_banner') ?>");
+                    background: url("<?php the_field('imagem_banner') ?>") no-repeat;
                 }
             </style>
         </header>
@@ -39,33 +39,56 @@
                         <div class="col-md-5">
                             <div class="titulo">
                                 <h1>
-                                    <?php the_field('desafio'); ?>
+                                    <?php the_field('titulo1'); ?>
                                     <div class="underbar"></div>
                                 </h1>
                             </div>
                             <div class="texto">
-                                <?php the_field('texto_desafio'); ?>
+                                <?php the_field('texto_1'); ?>
                             </div>
                         </div>
 
                         <div class="col-md-5" id="coluna_dois">
                             <div class="titulo">
                                 <h1>
-                                    <?php the_field('solucao'); ?>
+                                    <?php the_field('titulo2'); ?>
                                     <div class="underbar"></div>
                                 </h1>
                             </div>
                             <div class="texto">
-                                <?php the_field('texto_solucao'); ?>
+                                <?php the_field('texto_2'); ?>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
             <div class="imagem1">
                 <div class="container">
                     <div class="col-md-12 img-area">
                         <img src="<?php the_field('img1') ?>" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div id="texto2">
+                <div class="container">
+                    <div class="row ">
+                        <div class="col-md-5">
+                            <div class="titulo">
+                                <h1>
+                                    <?php the_field('titulo3'); ?>
+                                    <div class="underbar"></div>
+                                </h1>
+                            </div>
+                            <div class="texto">
+                                <?php the_field('texto_3'); ?>
+                            </div>
+                        </div>
+
+                        <div class="col-md-7 img-area">
+                            <img src="<?php the_field('img2') ?>" alt="">
+                        </div>
                     </div>
                 </div>
             </div>
